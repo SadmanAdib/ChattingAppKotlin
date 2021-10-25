fcdae3478cd1456519e0f22ac0eba225a1cccd42
SadmanAdib <56638634+SadmanAdib@users.noreply.github.com>
2021-10-26 01:49:02 +0600

    Connected to Firebase Completed sign in and sign up methods Enabled internet permissions and connectivity

--- app/src/main/java/com/adib/chattingappkotlin/Login.kt
+++ app/src/main/java/com/adib/chattingappkotlin/Login.kt
@@ -4,16 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
 
     private lateinit var btn_signup: Button
+    private lateinit var edt_email: EditText
+    private lateinit var edt_pasword: EditText
+    private lateinit var btn_login: Button
+
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         btn_signup=findViewById(R.id.btn_signup)
+        edt_email=findViewById(R.id.edt_email)
+        edt_pasword=findViewById(R.id.edt_password)
+        btn_login=findViewById(R.id.btn_login)
+
+        auth = FirebaseAuth.getInstance()
 
 
         btn_signup.setOnClickListener {
@@ -23,6 +37,32 @@ class Login : AppCompatActivity() {
 
         }
 
+        btn_login.setOnClickListener {
+            val email = edt_email.text.toString()
+            val password = edt_pasword.text.toString()
+
+            if(email == "" && password =="")
+            {
+                Toast.makeText(baseContext, "Check Fields", Toast.LENGTH_SHORT).show()
+            }
+            else login(email, password)
+
+        }
+
+
+    }
+
+    private fun login(email: String, password: String) {
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent (this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "User not found", Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 }
--- app/src/main/java/com/adib/chattingappkotlin/SignUp.kt
+++ app/src/main/java/com/adib/chattingappkotlin/SignUp.kt
@@ -1,24 +1,61 @@
 package com.adib.chattingappkotlin
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
 
     private lateinit var btn_signup: Button
+    private lateinit var edt_name: EditText
+    private lateinit var edt_email: EditText
+    private lateinit var edt_password: EditText
+
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         btn_signup = findViewById(R.id.btn_signup)
+        edt_name = findViewById(R.id.edt_name)
+        edt_email = findViewById(R.id.edt_email)
+        edt_password = findViewById(R.id.edt_password)
+
+        auth = FirebaseAuth.getInstance()
 
         btn_signup.setOnClickListener {
             val intent = Intent (this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        btn_signup.setOnClickListener {
+            val email = edt_email.text.toString()
+            val password = edt_password.text.toString()
+
+            signUp(email, password)
+
+        }
+
+    }
+
+    private fun signUp(email: String, password: String) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent (this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Sign Up Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 }
